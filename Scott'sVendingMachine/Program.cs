1c5f29f0606bddd2dd95945f78a612fe3c9c5718
Daniel Scott <daniel.s.arnfinsen@gmail.com>
2022-07-06 15:42:37 +0200

    Add project files.

---
+++ Scott'sVendingMachine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Scott_sVendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
